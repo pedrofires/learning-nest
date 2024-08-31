493f57cc6a21aa789facdcfeb315ddb947f2b3cb
Pedro Fires <pedrof.fires@gmail.com>
2024-08-31 18:45:13 -0300

    feat: add service layer

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,31 +1,33 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Put } from '@nestjs/common';
+import { CoursesService } from './courses.service';
 
 @Controller('courses')
 export class CoursesController {
+   constructor(private readonly coursesService: CoursesService) { }
+
    @Get()
    findAll() {
-      return 'Returns all courses'
+      return this.coursesService.findAll();
    }
 
    @Get(':id')
-   findOne(@Param('id') id: string) {
-      return `Returning ${id}`
+   findOne(@Param('id') id: number) {
+      return this.coursesService.findOne(+id);
    }
 
    @Post()
    create(@Body() body) {
-      return body;
+      return this.coursesService.create(body);
    }
 
-   @Patch(':id')
-   update(@Param('id') id: string, @Body() body) {
-      console.log(body);
-      return `Updated ${id}`;
+   @Put(':id')
+   update(@Param('id') id: number, @Body() body) {
+      return this.coursesService.update(+id, body);
    }
 
    @HttpCode(HttpStatus.NO_CONTENT)
    @Delete(':id')
-   remove(@Param('id') id: string) {
-      return `Deleted ${id}`;
+   remove(@Param('id') id: number) {
+      return this.coursesService.remove(+id);
    }
 }
--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,4 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Course } from './courses.entity';
 
 @Injectable()
-export class CoursesService {}
+export class CoursesService {
+   private courses: Course[] = [
+      {
+         id: 1,
+         name: "Curso NestJS Inciante",
+         description: "Curso sobre fundamentos do nestJs",
+         tags: ['nestjs', 'nodejs', 'typescript', 'javascript', 'backend']
+      }
+   ];
+
+   findAll() {
+      return this.courses;
+   }
+
+   findOne(id: number): Course {
+      const course = this.courses.find(course => course.id === id);
+      if (!course) {
+         throw new NotFoundException(`Course ID not found`);
+      }
+      return course;
+   }
+
+   create(createCourseDTO: any) {
+      this.courses.push(createCourseDTO);
+   }
+
+   update(id: number, updateCourseDTO: any) {
+      const courseFounded = this.findOne(id);
+      if (courseFounded) {
+         const index = this.courses.findIndex(course => course.id === id);
+         this.courses[index] = {
+            id,
+            ...updateCourseDTO
+         }
+      }
+   }
+
+   remove(id: number) {
+      const index = this.courses.findIndex(course => course.id === id);
+      if (index >= 0) {
+         this.courses.splice(index, 1);
+      }
+   }
+}
