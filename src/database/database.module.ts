bc29a6587304bacc83f129fccfb345b1dd81f3e9
Pedro Fires <pedrof.fires@gmail.com>
2024-09-08 23:38:41 -0300

    chore: configuring database to use nest package nest/typeorm

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,8 +1,29 @@
 import { Module } from '@nestjs/common';
-import { databaseProviders } from './database.providers';
+import { DataSourceOptions } from 'typeorm';
+import { Course } from '../courses/entities/courses.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+export const dataSourceOptions: DataSourceOptions = {
+   type: 'postgres',
+   host: 'localhost',
+   port: 5432,
+   username: 'postgres',
+   password: 'docker',
+   database: 'learningnest',
+   entities: [Course],
+   synchronize: true,
+}
 
 @Module({
-   providers: [...databaseProviders],
-   exports: [...databaseProviders],
+   imports: [
+      TypeOrmModule.forRootAsync({
+         useFactory: async () => {
+            return {
+               ...dataSourceOptions
+            }
+         }
+      })
+   ]
+
 })
 export class DatabaseModule { }
