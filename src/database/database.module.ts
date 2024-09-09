015725b8ca1cbbfb02386237af34b249c98a7a6b
Pedro Fires <pedrof.fires@gmail.com>
2024-09-08 23:47:12 -0300

    chore: adding tag entity

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { DataSourceOptions } from 'typeorm';
 import { Course } from '../courses/entities/courses.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Tag } from 'src/courses/entities/tags.entity';
 
 export const dataSourceOptions: DataSourceOptions = {
    type: 'postgres',
@@ -10,7 +11,7 @@ export const dataSourceOptions: DataSourceOptions = {
    username: 'postgres',
    password: 'docker',
    database: 'learningnest',
-   entities: [Course],
+   entities: [Course, Tag],
    synchronize: true,
 }
 
